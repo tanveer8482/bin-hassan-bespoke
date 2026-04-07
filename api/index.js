f48d74c37d50a070f4ab70d0104d79c0822c37b4
tanveer8482 <tanveer8482@gmail.com>
2026-04-07 07:39:39 -0700

    unified api gateway via agent

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const { URL } = require("url");
+
+function sendJson(res, status, body) {
+  res.statusCode = status;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(body));
+}
+
+const handlers = {
+  shops: require("../server/api/shops")
+};
+
+module.exports = async function (req, res) {
+  const url = new URL(req.url || "/", "http://localhost");
+  const segments = url.pathname.replace(/^\/+/g, "").split("/").filter(Boolean);
+  const route = segments[0] === "api" ? segments[1] : segments[0];
+
+  if (!route || !handlers[route]) {
+    return sendJson(res, 404, { error: "Not found" });
+  }
+
+  return handlers[route](req, res);
+};
